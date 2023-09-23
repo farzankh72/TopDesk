19e71a8204ed24fc353e6b7a86cd922d48f60d8b
Farzan khamse <43580634+farzankh72@users.noreply.github.com>
2023-09-24 03:17:45 +0330

    Joke wrapper created Js of integrated as one Fetching data by category added

--- Pages/ListOfCategories.js
+++ Pages/ListOfCategories.js
@@ -1,6 +1,5 @@
 const ListOfCategories = async () => {
     const URL_JOKE_CAT = 'https://api.chucknorris.io/jokes/categories';
-
     fetch(URL_JOKE_CAT, {
         method: 'GET',
         headers: {
@@ -11,7 +10,11 @@ const ListOfCategories = async () => {
             return resp.json();
         })
         .then((CategoriesData) => {
+
+            RandomJoke()
+
             const container = document.querySelector('.body-Container');
+
 
             const ListOfCatWrapper = document.createElement('ul')
 
@@ -28,9 +31,13 @@ const ListOfCategories = async () => {
                 const listOfCategory = document.createElement('li');
                 listOfCategory.style.display = 'inline-block';
                 listOfCategory.style.backgroundColor = 'lightBlue'
+                listOfCategory.style.cursor = 'pointer'
 
                 listOfCategory.textContent = item + ' / ';
                 ListOfCatWrapper.appendChild(listOfCategory);
+                listOfCategory.addEventListener('click', () => {
+                    Joke(item)
+                })
             });
             container.appendChild(ListOfCatWrapper)
         })
@@ -40,3 +47,51 @@ const ListOfCategories = async () => {
 }
 
 ListOfCategories()
+
+const Joke = (category) => {
+    const URL_JOKE = `https://api.chucknorris.io/jokes/random?category=${category}`;
+
+    const content = document.querySelector('.joke-content')
+    content.textContent = 'loading'
+    const createdAt = document.querySelector('.joke-creationTime')
+    createdAt.textContent = 'loading'
+    const jokeCat = document.querySelector('.joke-category')
+    jokeCat.textContent = 'loading'
+
+    if (category) {
+        fetch(URL_JOKE, {
+            method: 'GET', headers: {
+                'Content-Type': 'application/json',
+            }
+        }).then((res) => {
+            jokeCat.textContent = ''
+            return res.json()
+        }).then((joke) => {
+            content.textContent = joke.value
+            createdAt.textContent = joke.created_at
+            joke.categories.map((category) => {
+                jokeCat.textContent = jokeCat.textContent + category + '/'
+            })
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
+}
+
+const RandomJoke = () => {
+    const URL_RANDOM_JOKE = 'https://api.chucknorris.io/jokes/random'
+
+    const content = document.querySelector('.joke-content')
+
+    fetch(URL_RANDOM_JOKE, {
+        method: 'GET', headers: {
+            'Content-Type': 'application/json',
+        }
+    }).then((resp) => {
+        return resp.json()
+    }).then((joke) => {
+        content.textContent = joke.value
+    }).catch((err) => {
+        console.log(err)
+    })
+}
